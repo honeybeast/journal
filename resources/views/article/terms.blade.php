846e44d9510571061a21cda0679ea007f77b08c2
honeybeast <babystar.shine2019@gmail.com>
2019-10-14 06:57:30 +0300

    p

--- resources/views/article/terms.blade.php
+++ resources/views/article/terms.blade.php
@@ -40,8 +40,23 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-                    <p>
-                        An SSH key has been generated. You can download it below. You will need to add the generated public key to your Git vendor (such as Bitbucket, Github, Assembla or others). Once you have added the public key, insert your repository address and click Authenticate. This will pull the branch(es) name (master will be selected as default). To deploy the code click Start Deployment. You are done! From now onwards, you can simply pull your latest changes whenever you want.
+                    <p style="word-wrap: break-word;">
+                        Dear Editor,
+
+
+
+                        As a corresponding author, I declare that the said paper submitted for publication by Affirm publications is my original research work, it does not infringe any personal or property rights of another, the work does not contain
+                        anything libelous or otherwise illegal, and that the journal has the full power
+                        to enter into this agreement and assignment. I also agree that the work
+                        contains no material from other works protected by copyright that have been
+                        used without the written consent of the copyright owner, or that I can provide
+                        copies of all such required written consents to journal upon request. I have
+                        properly cited the work/s of others in the text as well as in the reference
+                        list. I further declare that the manuscript that the submitted for publication
+                        in the said journal, has not been previously published and is not currently
+                        under review elsewhere. If the article was prepared jointly with other authors,
+                        I have informed the co-author of the terms of this publishing agreement and
+                        that I am signing on their behalf as their agent, and I am authorized to do so.
                     </p>
                 </div>
             </div>
