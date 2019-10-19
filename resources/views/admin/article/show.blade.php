1285e9712aa30673cc80aab6e77682bbcaa70883
honeybeast <babystar.shine2019@gmail.com>
2019-10-19 09:39:02 +0300

    javascript

--- resources/views/admin/article/show.blade.php
+++ resources/views/admin/article/show.blade.php
@@ -228,15 +228,3 @@
         </div>
     </div>
 @endsection
-<script type="text/javascript">
-  (function($){
-    $(document).ready(function(){
-      $("#comment_submit").click(function() {
-        if($.trim($(".comment").val()) == "")
-        {
-          setTimeout(function() { $(".provider-site-wrap").hide(); }, 100);
-        }
-      });
-    });
-  })(jQuery);
-</script>
